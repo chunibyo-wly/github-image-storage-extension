cd40c4008019739ea426abf7594b8ad2eb2cf8df
chunibyo-wly <chunibyo.wly@gmail.com>
2020-06-25 19:34:56 +0800

    chore: init

---
+++ js/background.js
@@ -0,0 +1,16 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+    let w = 800;
+    let h = 550;
+    let left = Math.round((screen.width / 2) - (w / 2));
+    let top = Math.round((screen.height / 2) - (h / 2));
+
+    chrome.windows.create({
+        url: 'popup.html',
+        width: w,
+        height: h,
+        focused: true,
+        left: left,
+        top: top,
+        type: 'popup'
+    });
+});
